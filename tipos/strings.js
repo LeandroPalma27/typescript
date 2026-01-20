6346260cabc3ca2ebd8ebb95be9edc228ed0adae
LeandroPalma27 <psychomusicbox@gmail.com>
2026-01-20 17:05:45 -0500

    avance de clases

--- tipos/strings.js
+++ tipos/strings.js
@@ -4,6 +4,7 @@
     var batman = 'Batman';
     var greenLatern = 'Linterna verde';
     var volcanNegro = "Heroe: Linterna verde";
+    console.log("I'm ".concat(batman));
     console.log(batman.toUpperCase());
     console.log(((_a = batman[10]) === null || _a === void 0 ? void 0 : _a.toUpperCase()) || "No hay un 10mo caracter.");
 })();
--- tipos/strings.ts
+++ tipos/strings.ts
@@ -3,6 +3,8 @@
     const greenLatern: string = 'Linterna verde';
     const volcanNegro: string = `Heroe: Linterna verde`;
 
+    console.log(`I'm ${batman}`);
+
     console.log(batman.toUpperCase());
     console.log(batman[10]?.toUpperCase() || "No hay un 10mo caracter.");
 })();
